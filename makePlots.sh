0fc66d3213a83c1cc4fd7de56dc50926a347b6d4
amsutton <amsutton@d.umn.edu>
2026-04-17 10:41:45 -0500

    script for pgfplots data

---
+++ makePlots.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+## n,q,t
+
+function make_table_by_q {
+    echo "n mean sd"
+    cat $1 | sort -t',' -k1,2 -n | awk -F, -v q="$2" '$2 == q{print $0}' | datamash -t, groupby 1 mean 3 sstdev 3 --output-delimiter=' '
+}
+
+make_table_by_q "results.txt" 2 > cycle_mutation_q2.dat
+make_table_by_q "results.txt" 3 > cycle_mutation_q3.dat
+make_table_by_q "results.txt" 4 > cycle_mutation_q4.dat
+make_table_by_q "results.txt" 5 > cycle_mutation_q5.dat
+
